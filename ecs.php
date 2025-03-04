418ea8320489d99ed820fc2c6f3dd3fe51e7da2e
janpalen <jan.palen@bitbag.pl>
2025-03-04 13:08:03 +0100

    Fix ECS

--- ecs.php
+++ ecs.php
@@ -1,21 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
-use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests/Behat',
-        __DIR__ . '/ecs.php',
-    ]);
+return static function (ECSConfig $config): void {
+    putenv('ALLOW_BITBAG_OS_HEADER=1');
 
-    $ecsConfig->import('vendor/sylius-labs/coding-standard/ecs.php');
-
-    $ecsConfig->skip([
-        VisibilityRequiredFixer::class => ['*Spec.php'],
-    ]);
+    $config->import('vendor/bitbag/coding-standard/ecs.php');
+    $config->paths(['src', 'tests']);
 };
-
